3883d2705cead2ba754153e39f662cd9cbabf482
cryptodechange <tommeh@hotmail.co.uk>
2013-12-20 00:47:51 +0000

    changed port to prevent conflict

--- backends/irc/__init__.py
+++ backends/irc/__init__.py
@@ -57,7 +57,7 @@ class IrcThread(threading.Thread):
             s += 'p' + self.pruning_limit + ' '
 
         def add_port(letter, number):
-            DEFAULT_PORTS = {'t':'50001', 's':'50002', 'h':'8081', 'g':'8082'}
+            DEFAULT_PORTS = {'t':'50501', 's':'50502', 'h':'8581', 'g':'8582'}
             if not number: return ''
             if DEFAULT_PORTS[letter] == number:
                 return letter + ' '
